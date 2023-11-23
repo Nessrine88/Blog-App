1b682463cd809e0953241dbcc2e2c8255a4a4204
unknown <macherki.nessrine@yahoo.com>
2023-11-23 22:40:05 +0100

    fix linters

--- spec/system/users_index_spec.rb
+++ spec/system/users_index_spec.rb
@@ -1,5 +1,4 @@
 require 'rails_helper'
-
 describe 'user index', type: :system do
   let!(:user1) { User.create(name: 'Alice', bio: 'Writer', photo: 'https://images.unsplash.com/photo-1508921912186-1d1a45ebb3c1?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D') }
   it 'displays the username of all other users' do
