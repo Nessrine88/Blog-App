72f7c65f7d95b09dee1c74a85f714e4f67186dbc
unknown <macherki.nessrine@yahoo.com>
2023-11-25 13:06:07 +0100

    add confirmation

--- db/migrate/20231026201955_add_index_to_posts.rb
+++
@@ -1,5 +0,0 @@
-class AddIndexToPosts < ActiveRecord::Migration[7.1]
-  def change
-    add_index :posts, :author_id
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -36,9 +36,9 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_23_130325) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "text"
-    t.integer "comments_counter"
-    t.integer "likes_counter"
-    t.integer "author_id", null: false
+    t.bigint "author_id", null: false
+    t.integer "comments_counter", default: 0
+    t.integer "likes_counter", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["author_id"], name: "index_posts_on_author_id"
@@ -47,15 +47,21 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_23_130325) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "photo"
-    t.string "bio"
+    t.text "bio"
     t.integer "posts_counter"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "first_name", default: "", null: false
+    t.string "last_name", default: "", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
